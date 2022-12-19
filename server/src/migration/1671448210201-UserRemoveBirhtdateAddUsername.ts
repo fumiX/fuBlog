fa92bc8d89c1ae52217602cd2ae48dcb03438365
Florian Schäfer <florian.schaefer@fumix.de>
2022-12-19 12:34:49 +0100

    Add missing migration

---
+++ server/src/migration/1671448210201-UserRemoveBirhtdateAddUsername.ts
@@ -0,0 +1,52 @@
+import { MigrationInterface, QueryRunner, TableColumn } from "typeorm"
+
+export class UserRemoveBirhtdateAddUsername1671448210201 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.changeColumns(
+            "user",
+            [
+                {
+                    oldColumn: new TableColumn({
+                        name: "birthdate",
+                        type: "date",
+                    }),
+                    newColumn: new TableColumn({
+                        name: "username",
+                        length: "64",
+                        type: "varchar",
+                        isNullable: true,
+                        isUnique: true,
+                    })
+                }
+            ]
+        );
+        await queryRunner.query(`UPDATE "user" SET "username" = "user"."firstName" || ' ' || "user"."lastName"${""}`);
+        await queryRunner.changeColumns(
+            "user",
+            [
+                {
+                    oldColumn: new TableColumn({
+                        name: "username",
+                        length: "64",
+                        type: "varchar",
+                        isNullable: true,
+                        isUnique: true,
+                    }),
+                    newColumn: new TableColumn({
+                        name: "username",
+                        length: "64",
+                        type: "varchar",
+                        isNullable: false,
+                        isUnique: true,
+                    })
+                }
+            ]
+        );
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query("SELECT 'TODO: Implement!'")
+    }
+
+}
