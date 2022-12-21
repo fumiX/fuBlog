18e4d7550ee4bc92a71f0694f10ab71a1a37eb2a
Florian Schäfer <florian.schaefer@fumix.de>
2022-12-21 09:59:20 +0100

    Improve docker buildscript

--- build-docker-images.sh
+++ build-docker-images.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 SCRIPT_RELATIVE_DIR=$(dirname "${BASH_SOURCE[0]}")
-cd $SCRIPT_RELATIVE_DIR
+cd "$SCRIPT_RELATIVE_DIR" || exit
 
 
-npm install
+npm install || exit
 rm -r server/dist/
-npm run build
+npm run build || exit
 cp "docker/app/Dockerfile" "server/dist/"
 cp "server/package.json" "server/dist/"
 mkdir -p "server/dist/node_modules/@fumix/"
 cp -r -T "common/" "server/dist/node_modules/@fumix/fu-blog-common/"
-cd "server/dist/"
-docker build .
+cd "server/dist/" || exit
+docker build -t fu-blog:latest .
