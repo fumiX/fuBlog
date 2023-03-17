f5a13cdc6fbb5f773ebd8b987ade232a6200aeba
Thomas Haaf <thomas.haaf@fumix.de>
2023-03-17 16:18:15 +0100

    default locale is DE

--- client/src/plugins/i18n.ts
+++ client/src/plugins/i18n.ts
@@ -4,7 +4,7 @@ import { createI18n } from "vue-i18n";
 
 const i18n = createI18n({
   legacy: false,
-  locale: import.meta.env.VITE_I18N_LOCALE || "en",
+  locale: import.meta.env.VITE_I18N_LOCALE || "de",
   fallbackLocale: import.meta.env.VITE_I18N_FALLBACK_LOCALE || "en",
   messages: { de, en },
 });
