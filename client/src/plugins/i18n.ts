224399854261fd065ea3d2fed16d8a2dfd513436
Thomas Haaf <thomas.haaf@fumix.de>
2023-03-20 14:37:01 +0100

    fix console i18n warnings

--- client/src/plugins/i18n.ts
+++ client/src/plugins/i18n.ts
@@ -4,6 +4,8 @@ import { createI18n } from "vue-i18n";
 
 const i18n = createI18n({
   legacy: false,
+  missingWarn: false,
+  fallbackWarn: false,
   locale: import.meta.env.VITE_I18N_LOCALE || "de",
   fallbackLocale: import.meta.env.VITE_I18N_FALLBACK_LOCALE || "en",
   messages: { de, en },
