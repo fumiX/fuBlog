1e20e9a38ca38ce602c7f7e5baa8c21f6147098c
Christophe Bach <christophe.bach@fumix.de>
2023-03-21 10:47:40 +0100

    daily rotate logs

--- server/src/logger.ts
+++ server/src/logger.ts
@@ -1,20 +1,38 @@
-import { createLogger, format, transports } from "winston";
+import winston, { createLogger, format, transports } from "winston";
+import "winston-daily-rotate-file/index.js";
+
+const dateFormat = () => {
+  return new Date().toLocaleString("de-DE", {
+    timeZone: "Europe/Berlin",
+    year: "numeric",
+    month: "2-digit",
+    day: "numeric",
+    minute: "numeric",
+    second: "numeric",
+    hour: "2-digit",
+  });
+};
+
+const uncolored = format.combine(
+  format.timestamp({ format: dateFormat }),
+  format.printf(({ timestamp, level, message }) => {
+    return `[${timestamp}] ${level}: ${message}`;
+  }),
+);
 
 export const logger = createLogger({
   transports: [
-    new transports.Console(),
-    new transports.File({
+    new transports.Console({
+      format: format.combine(format.colorize(), uncolored),
+    }),
+    new winston.transports.DailyRotateFile({
       dirname: "logs",
-      filename: "node.log",
+      filename: "node-%DATE%.log",
+      datePattern: "YYYY-MM-DD",
+      maxFiles: "7d",
+      format: uncolored,
     }),
   ],
-  format: format.combine(
-    format.colorize(),
-    format.timestamp(),
-    format.printf(({timestamp, level, message}) => {
-      return `[${timestamp}] ${level}: ${message}`;
-    }),
-  ),
 });
 
 export default logger;
