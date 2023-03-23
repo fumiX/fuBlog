ee7286324875a4a76007d5eeee3a0cf229613fca
Christophe Bach <christophe.bach@fumix.de>
2023-03-23 11:58:29 +0100

    optimize error handling

--- server/src/routes/posts.ts
+++ server/src/routes/posts.ts
@@ -1,13 +1,11 @@
 import { DraftResponseDto } from "@fumix/fu-blog-common";
 import express, { Request, Response, Router } from "express";
-import { ParamsDictionary } from "express-serve-static-core";
 import multer from "multer";
 import { AppDataSource } from "../data-source.js";
 import { AttachmentEntity } from "../entity/Attachment.entity.js";
 import { PostEntity } from "../entity/Post.entity.js";
 import { UserEntity } from "../entity/User.entity.js";
 import { MarkdownConverterServer } from "../markdown-converter-server.js";
-import { BadRequestError } from "../types/errors/BadRequestError.js";
 import { NotFoundError } from "../types/errors/NotFoundError.js";
 
 const router: Router = express.Router();
@@ -16,7 +14,7 @@ const upload = multer();
 // create or get dummy user
 async function getUser() {
   const email = "test@test.de";
-  let createdUser = await AppDataSource.manager.getRepository(UserEntity).findOneBy({email: email});
+  let createdUser = await AppDataSource.manager.getRepository(UserEntity).findOneBy({ email: email });
 
   if (createdUser === null) {
     const user: UserEntity = {
@@ -32,76 +30,58 @@ async function getUser() {
   return createdUser;
 }
 
-function validateSearchParams(params: ParamsDictionary) {
-  if (params.page && (isNaN(+params.page) || +params.page < 1)) {
-    throw new BadRequestError("Page must be a positive number");
-  }
-  if (params.count && (isNaN(+params.count) || +params.count < 1)) {
-    throw new BadRequestError("Count must be a positive number");
-  }
-  if (params.operator && !["and", "or"].includes(params.operator)) {
-    throw new BadRequestError("Operator must be one of [and,or].");
-  }
-}
-
 // search posts
 router.get("/page/:page/count/:count/search/:search/operator/:operator", async (req: Request, res: Response, next) => {
-  try {
-    validateSearchParams(req.params);
-    const page = +req.params.page;
-    const itemsPerPage = +req.params.count;
-    const skipEntries = page * itemsPerPage - itemsPerPage;
-    let searchTerm = "";
-    if (req.params.search) {
-      const splitSearchParams: string[] = req.params.search.trim().split(" ");
-      const operator = req.params.operator === "or" ? " | " : " & ";
+  const page = +req.params.page;
+  const itemsPerPage = +req.params.count;
+  const skipEntries = page * itemsPerPage - itemsPerPage;
+  let searchTerm = "";
+  if (req.params.search) {
+    const splitSearchParams: string[] = req.params.search.trim().split(" ");
+    const operator = req.params.operator === "or" ? " | " : " & ";
 
-      searchTerm = splitSearchParams
-        .map((word) => escape(word))
-        .filter(Boolean)
-        .join(operator);
-    }
-
-    const allSearchedPosts = await AppDataSource.manager
-      .getRepository(PostEntity)
-      .createQueryBuilder()
-      .where("ts @@ to_tsquery(:searchTerm)", {searchTerm: searchTerm})
-      .skip(skipEntries)
-      .take(itemsPerPage)
-      // .orderBy("createdAt", "DESC")
-      .getManyAndCount();
-
-    res.status(200).json({data: allSearchedPosts});
-  } catch (e) {
-    next(e);
+    searchTerm = splitSearchParams
+      .map((word) => escape(word))
+      .filter(Boolean)
+      .join(operator);
   }
+
+  await AppDataSource.manager
+    .getRepository(PostEntity)
+    .createQueryBuilder()
+    .where("ts @@ to_tsquery(:searchTerm)", { searchTerm: searchTerm })
+    .skip(skipEntries)
+    .take(itemsPerPage)
+    // .orderBy("createdAt", "DESC")
+    .getManyAndCount()
+    .then((result) => res.status(200).json({ data: result }))
+    .catch((err) => next(err));
 });
 
 // get all posts with paging
-router.get("/page/:page/count/:count/", async (req: Request, res: Response, next) => {
-  try {
-    validateSearchParams(req.params);
-    const page = +req.params.page;
-    const itemsPerPage = +req.params.count;
-    const skipEntries = page * itemsPerPage - itemsPerPage;
-    const allPosts = await AppDataSource.manager.getRepository(PostEntity).findAndCount({
+router.get("/page/:page([0-9]+)/count/:count([0-9]+)/", async (req: Request, res: Response, next) => {
+  const page = +req.params.page;
+  const itemsPerPage = +req.params.count;
+  const skipEntries = page * itemsPerPage - itemsPerPage;
+  await AppDataSource.manager
+    .getRepository(PostEntity)
+    .findAndCount({
       order: {
         createdAt: "DESC",
       },
       skip: skipEntries,
       take: itemsPerPage,
       relations: ["createdBy", "updatedBy"],
+    })
+    .then((result) => res.status(200).json({ data: result }))
+    .catch((error) => {
+      next(error);
     });
-
-    res.status(200).json({data: allPosts});
-  } catch (e) {
-    next(e);
-  }
 });
 
 // GET POST BY ID WITH RELATIONS TO USER
-router.get("/:id", async (req: Request, res: Response, next) => {
-  const post = await AppDataSource.manager
+router.get("/:id([0-9]+)", async (req: Request, res: Response, next) => {
+  await AppDataSource.manager
     .getRepository(PostEntity)
     .findOne({
       where: {
@@ -113,7 +93,7 @@ router.get("/:id", async (req: Request, res: Response, next) => {
       if (result === null) {
         throw new NotFoundError("No post found with id " + req.params.id);
       } else {
-        res.status(200).json({ data: post });
+        res.status(200).json({ data: result });
       }
     })
     .catch((error) => {
@@ -131,7 +111,7 @@ function convertAttachment(filename: string, buffer: Buffer, mimeType: string, p
 }
 
 // CREATE NEW POST
-router.post("/new", upload.single("file"), async (req: Request, res: Response) => {
+router.post("/new", upload.single("file"), async (req: Request, res: Response, next) => {
   const bodyJson = JSON.parse(req.body.body);
   // TODO handle
   try {
@@ -148,7 +128,10 @@ router.post("/new", upload.single("file"), async (req: Request, res: Response) =
       attachments: [],
     };
 
-    const savedPost = await AppDataSource.manager.getRepository(PostEntity).save<PostEntity>(post);
+    const savedPost = await AppDataSource.manager
+      .getRepository(PostEntity)
+      .save<PostEntity>(post)
+      .catch((err) => next(err));
     const fileFromRequest = req.file;
     let attachmentEntity = null;
 
@@ -156,55 +139,65 @@ router.post("/new", upload.single("file"), async (req: Request, res: Response) =
     if (fileFromRequest) {
       // let attachments = files?.map((fileFromRequest: Multer.File) => convertAttachment(fileFromRequest.filename, fileFromRequest.buffer, fileFromRequest.mimetype, post));
       attachmentEntity = convertAttachment(fileFromRequest.originalname, fileFromRequest.buffer, fileFromRequest.mimetype, post);
-      await AppDataSource.manager.getRepository(AttachmentEntity).save<AttachmentEntity>(attachmentEntity);
-      savedPost.attachments.push(attachmentEntity);
+      await AppDataSource.manager
+        .getRepository(AttachmentEntity)
+        .save<AttachmentEntity>(attachmentEntity)
+        .catch((err) => next(err));
+      if (savedPost instanceof PostEntity) {
+        savedPost.attachments.push(attachmentEntity);
+      }
     }
-    let responseDto: DraftResponseDto;
+    let responseDto: DraftResponseDto = { attachments: [] };
     if (attachmentEntity !== null) {
-      responseDto = {
-        attachments: [
-          {
-            id: attachmentEntity?.id,
-            binaryData: attachmentEntity.binaryData,
-            mimeType: attachmentEntity.mimeType,
-            filename: attachmentEntity.filename,
-          },
-        ],
-        postId: savedPost.id,
-      };
+      if (savedPost instanceof PostEntity) {
+        responseDto = {
+          attachments: [
+            {
+              id: attachmentEntity?.id,
+              binaryData: attachmentEntity.binaryData,
+              mimeType: attachmentEntity.mimeType,
+              filename: attachmentEntity.filename,
+            },
+          ],
+          postId: savedPost.id,
+        };
+      }
     } else {
-      responseDto = {postId: savedPost.id, attachments: []};
+      if (savedPost instanceof PostEntity) {
+        responseDto = { postId: savedPost.id, attachments: [] };
+      }
     }
     res.status(200).send(responseDto);
   } catch (e) {
-    res.status(500).json({error: "Fehler " + e});
+    next(e);
   }
 });
 
 // EDIT EXISTING POST
-router.post("/:id", upload.single("file"), async (req: Request, res: Response, next) => {
+router.post("/:id([0-9]+)", upload.single("file"), async (req: Request, res: Response, next) => {
   const post = await AppDataSource.manager.getRepository(PostEntity).findOneBy({
     id: +req.params.id,
   });
 
   const bodyJson = JSON.parse(req.body.body);
   if (post) {
-    try {
-      await AppDataSource.manager
-        .createQueryBuilder()
-        .update("post")
-        .set({
-          title: bodyJson.title,
-          description: bodyJson.description,
-          markdown: bodyJson.markdown,
-          updatedAt: new Date(),
-          sanitizedHtml: await MarkdownConverterServer.Instance.convert(bodyJson.markdown),
-          updatedBy: await getUser(),
-          draft: bodyJson.draft,
-        })
-        .where("id = :id", {id: post.id})
-        .execute();
+    await AppDataSource.manager
+      .createQueryBuilder()
+      .update("post")
+      .set({
+        title: bodyJson.title,
+        description: bodyJson.description,
+        markdown: bodyJson.markdown,
+        updatedAt: new Date(),
+        sanitizedHtml: await MarkdownConverterServer.Instance.convert(bodyJson.markdown),
+        updatedBy: await getUser(),
+        draft: bodyJson.draft,
+      })
+      .where("id = :id", { id: post.id })
+      .execute()
+      .catch((err) => next(err));
 
+    try {
       const fileFromRequest = req.file;
       let attachmentEntity = null;
 
@@ -212,7 +205,10 @@ router.post("/:id", upload.single("file"), async (req: Request, res: Response, n
       if (fileFromRequest) {
         // let attachments = files?.map((fileFromRequest: Multer.File) => convertAttachment(fileFromRequest.filename, fileFromRequest.buffer, fileFromRequest.mimetype, post));
         attachmentEntity = convertAttachment(fileFromRequest.originalname, fileFromRequest.buffer, fileFromRequest.mimetype, post);
-        await AppDataSource.manager.getRepository(AttachmentEntity).save<AttachmentEntity>(attachmentEntity);
+        await AppDataSource.manager
+          .getRepository(AttachmentEntity)
+          .save<AttachmentEntity>(attachmentEntity)
+          .catch((err) => next(err));
         post.attachments = [];
         post.attachments.push(attachmentEntity);
       }
@@ -230,7 +226,7 @@ router.post("/:id", upload.single("file"), async (req: Request, res: Response, n
           postId: post.id,
         };
       } else {
-        responseDto = {postId: post.id, attachments: []};
+        responseDto = { postId: post.id, attachments: [] };
       }
       res.status(200).send(responseDto);
     } catch (e) {
@@ -240,16 +236,18 @@ router.post("/:id", upload.single("file"), async (req: Request, res: Response, n
 });
 
 // DELETE POST
-router.get("/delete/:id", async (req: Request, res: Response) => {
-  try {
-    // find all attachments of post and delete them
-    await AppDataSource.manager.getRepository(AttachmentEntity).delete({post: {id: +req.params.id}});
-    // find post in DB and delete it
-    const result = await AppDataSource.manager.getRepository(PostEntity).delete(+req.params.id);
-    res.status(200).send(result);
-  } catch (e) {
-    res.status(500).json({error: "Fehler " + e});
-  }
+router.get("/delete/:id([0-9]+)", async (req: Request, res: Response, next) => {
+  // find all attachments of post and delete them
+  await AppDataSource.manager
+    .getRepository(AttachmentEntity)
+    .delete({ post: { id: +req.params.id } })
+    .catch((err) => next(err));
+  // find post in DB and delete it
+  const result = await AppDataSource.manager
+    .getRepository(PostEntity)
+    .delete(+req.params.id)
+    .catch((err) => next(err));
+  res.status(200).send(result);
 });
 
 export default router;
