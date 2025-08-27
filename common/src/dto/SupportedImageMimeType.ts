a28a4fe7103ae7ef9734e91467696c2797047cef
Thomas Haaf <thomas@smart-sign.com>
2025-08-27 13:16:01 +0200

    revert mimetype

--- common/src/dto/SupportedImageMimeType.ts
+++ common/src/dto/SupportedImageMimeType.ts
@@ -2,6 +2,6 @@
  * The supported MIME types for attachments (for now).
  * Currently they can also all be auto-determined by the magic bytes.
  */
-export type SupportedImageMimeType = `image/${"png" | "jpeg" | "gif" | "bmp" | "webp" | "mp3"}`;
+export type SupportedImageMimeType = `image/${"png" | "jpeg" | "gif" | "bmp"}`;
 
 export type JsonMimeType = "application/json";
